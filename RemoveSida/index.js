9e35d84c759d1fcd2b229d6da5aadf01333fd340
Jud02 <judy.ynh@gmail.com>
2025-01-07 19:50:30 +0100

    Knappen "remove" för sida REMOVE
    Lägger till funktionalitet för att ta bort celler baserat på det slumpmässiga numret. När knappen "removeButton" klickas, markeras celler med det aktuella slumpmässiga numret genom att sätta deras text till "X" och ändra bakgrundsfärgen till röd. Antalet borttagna celler visas i ett element. Om inga celler tas bort visas ett meddelande om att det inte finns något att ta bort.

--- RemoveSida/index.js
+++ RemoveSida/index.js
@@ -19,3 +19,27 @@ newRandomButton.addEventListener("click", function () {
     }
 });
 
+
+const removeButton = document.getElementById("removeButton");
+const rightParagraph = document.getElementById("right");
+
+removeButton.addEventListener("click", function () {
+    const randomNum = +leftParagraph.textContent; 
+    const cells = document.querySelectorAll("#numbersDiv div"); 
+    let count = 0; 
+
+    for (let i = 0; i < cells.length; i++) { 
+        const cellNumber = +cells[i].textContent; 
+        if (cellNumber === randomNum) { 
+            cells[i].textContent = "X"; 
+            cells[i].style.backgroundColor = "#8C030E"; 
+            count++;
+        }
+    }
+
+    if (count > 0) {
+        rightParagraph.textContent = `${randomNum} was removed ${count} time(s).`;
+    } else {
+        rightParagraph.textContent = "Nothing to remove"; 
+    }
+});
