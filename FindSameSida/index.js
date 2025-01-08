a1fc8e8a706a874f081971d71a92036920e7a8d7
Jud02 <judy.ynh@gmail.com>
2025-01-08 19:18:44 +0100

    Lade till funktion för att lägga till klick-händelser på celler
    - Skapade funktionen `addEventListenersToCells` för att lägga till en klick-händelse på varje cell i `#numbersDiv`.
    - Vid klick på en cell anropas `handleCellClick` för att hantera markering och räkning av matchande nummer.

--- FindSameSida/index.js
+++ FindSameSida/index.js
@@ -1,4 +1,9 @@
 
+document.addEventListener("DOMContentLoaded", function () {
+    const resetButton = document.getElementById("resetButton");
+    const pElement = document.querySelector("#howMany");
+
+
     function handleCellClick(cell, cells) {
         const number = cell.textContent;
         let count = 0;
@@ -20,3 +25,13 @@
     function updateCountText(number, count) {
         pElement.textContent = count + " copies of the number " + number;
     }
+
+    function addEventListenersToCells() {
+        const cells = document.querySelectorAll("#numbersDiv div");
+        for (let i = 0; i < cells.length; i++) {
+            cells[i].addEventListener("click", function () {
+                handleCellClick(cells[i], cells);
+            });
+        }
+    }
+
